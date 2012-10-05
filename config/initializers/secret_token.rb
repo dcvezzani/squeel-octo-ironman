7b9e77ba5d80d053f46c15e4f0d2a9b516127217
David Vezzani <david_vezzani@yahoo.com>
2012-10-05 13:04:59 -0700

    rails baseline install

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SqueelOctoIronman::Application.config.secret_token = '46344bd948b367763c18bd705f2940e5727f10ed9dec8cb220238bf8a01bfdd2d068db1f206d2910d79363d1a7b26477ab0011afe4c6b407b2f0597b24030c09'
